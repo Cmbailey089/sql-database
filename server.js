f21eecc37a5ad69090204f7a38a72f801083da8d
Cmbailey089 <christopherbailey089@gmail.com>
2023-05-02 18:40:54 -0400

    localhost added

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-const cTable = require('console.table');
 const express = require('express');
-const mysql= require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res) => {
+    res.status(404).end();
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}`);
+  });
